acdad290e30bb373408d58db97a22a19a066a938
Pedro Carvalho <ppscarvalho@gmail.com>
2023-05-13 06:03:24 -0300

    Implementa projeto SM.People.Apresentation.Api da Soluction SM.People

---
+++ src/Apresentation/SM.People.Apresentation.Api/Controllers/RPCSupplierController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SM.MQ.Models;
+using SM.MQ.Models.Supplier;
+using SM.MQ.Operators;
+using SM.People.Core.Application.Models;
+using SM.People.Core.Application.Queries.Supplier;
+
+namespace SM.Catalog.Apresentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RPCSupplierController : ControllerBase
+    {
+        private readonly IPublish _publish;
+
+        public RPCSupplierController(IPublish publish)
+        {
+            _publish = publish;
+        }
+
+        [HttpPost]
+        [Route("GetSupplierById")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ResponseSupplierOut), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetSupplierById([FromBody] GetSupplierByIdQuery query)
+        {
+            var mapIn = new RequestIn
+            {
+                Host = "localhost",
+                Result = query.Id.ToString(),
+                Queue = "GetSupplierById"
+            };
+
+            var result = await _publish.DoRPC<RequestIn, ResponseSupplierOut>(mapIn);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("GetAllSupplier")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ResponseSupplierOut), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllSupplier()
+        {
+            var mapIn = new RequestIn
+            {
+                Host = "localhost",
+                Queue = "GetAllSupplier"
+            };
+
+            var result = await _publish.DoRPC<RequestIn, ResponseSupplierOut[]>(mapIn);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("AddSupplier")]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseOut>> AddSupplier([FromBody] SupplierModel SupplierModel)
+        {
+            var mapIn = new RequestIn
+            {
+                Host = "localhost",
+                Result = JsonConvert.SerializeObject(SupplierModel),
+                Queue = "AddSupplier"
+            };
+
+            var response = await _publish.DoRPC<RequestIn, ResponseOut>(mapIn);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("UpdateSupplier")]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseOut), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseOut>> UpdateSupplier([FromBody] SupplierModel SupplierModel)
+        {
+            var mapIn = new RequestIn
+            {
+                Host = "localhost",
+                Result = JsonConvert.SerializeObject(SupplierModel),
+                Queue = "UpdateSupplier"
+            };
+
+            var response = await _publish.DoRPC<RequestIn, ResponseOut>(mapIn);
+            return response;
+        }
+    }
+}
---
+++ src/Apresentation/SM.People.Apresentation.Api/Controllers/SupplierController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SM.Catalog.Apresentation.Api.Controllers.BaseController;
+using SM.People.Core.Application.Commands.Supplier;
+using SM.People.Core.Application.Models;
+using SM.People.Core.Application.Queries.Supplier;
+using SM.Resource.Communication.Mediator;
+using SM.Resource.Messagens.CommonMessage.Notifications;
+using SM.Resource.Util;
+
+namespace SM.Catalog.Apresentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupplierController : ControllerConfiguration
+    {
+        private readonly ILogger<SupplierController> _logger;
+        private readonly IMediatorHandler _mediatorHandler;
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediatorQuery;
+
+        public SupplierController(
+            ILogger<SupplierController> logger,
+            INotificationHandler<DomainNotification> notifications,
+            IMediatorHandler mediatorHandler,
+            IMapper mapper,
+            IMediator mediatorQuery) : base(notifications, mediatorHandler)
+        {
+            _logger = logger;
+            _mediatorHandler = mediatorHandler;
+            _mapper = mapper;
+            _mediatorQuery = mediatorQuery;
+        }
+
+        [HttpPost]
+        [Route("GetSupplierById")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(SupplierModel), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetSupplierById([FromBody] GetSupplierByIdQuery query)
+        {
+            _logger.LogInformation("Obter todas as categorias");
+            var result = await _mediatorQuery.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("GetAllSupplier")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(SupplierModel), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllSupplier()
+        {
+            _logger.LogInformation("Obter todas as categorias");
+            var result = await _mediatorQuery.Send(new GetAllSupplierQuery());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("AddSupplier")]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<DefaultResult>> SaveSupplier([FromBody] SupplierModel SupplierModel)
+        {
+            var cmd = _mapper.Map<AddSupplierCommand>(SupplierModel);
+            var result = await _mediatorHandler.SendCommand(cmd);
+
+            if (ValidOperation())
+                return Ok(result);
+            else
+                return BadRequest(GetMessageError());
+        }
+
+        [HttpPost]
+        [Route("UpdateSupplier")]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(DefaultResult), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<DefaultResult>> UpdateSupplier([FromBody] SupplierModel SupplierModel)
+        {
+            var cmd = _mapper.Map<UpdateSupplierCommand>(SupplierModel);
+            var result = await _mediatorHandler.SendCommand(cmd);
+
+            if (ValidOperation())
+                return Ok(result);
+            else
+                return BadRequest(GetMessageError());
+        }
+    }
+}
